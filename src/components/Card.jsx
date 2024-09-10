4e0f546955c36bc922705cfe084beb1038b4137b
rlores <raquel.lores@edison.es>
2024-09-10 09:08:19 +0200

    create_repo

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+
+const Card = () => {
+  return (
+    <div >
+<div className="card" style="width: 18rem;">
+  <img src="..." className="card-img-top" alt="..."></img>
+  <div className="card-body">
+    <h5 className="card-title">Chart Title</h5>
+    <p className="card-text">Chart explanation</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Card
