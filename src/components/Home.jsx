8a28e60974d9ba0281ff7df2aa2ebb2c289363e3
rlores <raquel.lores@edison.es>
2024-09-13 14:30:03 +0200

    install_Tailwind

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import Map from "./Map";
 export function Home() {
   return (
     <div>
-    <h1>Bienvenidos a la página principal</h1>
+    <h1 className="text-3xl font-bold underline">Bienvenidos a Edison Next Spain con Tailwind</h1>
     <Map />
     </div>
   );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,34 +4,29 @@ const Map = () => {
   useEffect(() => {
     let map;
 
-    // Función para inicializar el mapa
-    const initMap = () => {
+      const initMap = () => {
       map = new window.google.maps.Map(document.getElementById("map"), {
-        center: { lat: 40.4057, lng: -3.6708 }, // Centro inicial del mapa
+        center: { lat: 40.4057, lng: -3.6708 }, 
         zoom: 8,
       });
 
-      // Cargar datos de dbgeo.json
       fetch("../../server/dbgeo.json")
         .then((response) => response.json())
         .then((data) => {
-          // Iterar sobre los sitios y agregar marcadores
           data.sites.forEach((site) => {
             const coords = site.geometry.coordinates;
             const latLng = new window.google.maps.LatLng(coords[1], coords[0]);
 
-            // Crear marcador
             new window.google.maps.Marker({
               position: latLng,
-              map: map, // Usar el objeto del mapa aquí
-              title: site.properties.name, // Añadir nombre del sitio
+              map: map,
+              title: site.properties.name, 
             });
           });
         })
         .catch((error) => console.error("Error al cargar dbgeo.json:", error));
     };
 
-    // Cargar el script de Google Maps
     if (!window.google) {
       const script = document.createElement("script");
       script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDgXksZ9VmsHXH2KD1_q4qa8gzsXr24-zA&callback=initMap`;
@@ -43,7 +38,6 @@ const Map = () => {
       window.initMap();
     }
 
-    // Limpiar el script solo si fue agregado correctamente
     return () => {
       const scriptTag = document.querySelector(`script[src*="maps.googleapis.com"]`);
       if (scriptTag) {
